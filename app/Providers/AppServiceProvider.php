4874142355a9d9f99b8d8301fe7adf9a644ce6c2
Truong <quangdao@gmail.com>
2026-06-25 15:38:44 +0700

    force https service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-
+        URL::forceScheme('https');
         Paginator::defaultView('pagination::bootstrap-5');
     }
 }
